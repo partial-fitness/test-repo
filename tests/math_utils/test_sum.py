2c946be12e20ca68f634381caa1b897c8f37d0c4
Emanuel de Souza <emanuelcds@gmail.com>
2018-12-27 15:43:01 -0800

    test(TetSum): added negative and neutral sum scenario.

--- tests/math_utils/test_sum.py
+++ tests/math_utils/test_sum.py
@@ -7,4 +7,11 @@ class TestSum:
         neutral_element = 0
         result = sum(positive_int, neutral_element)
 
-        assert result > 0
+        assert result == 100
+
+    def test_when_passed_negative_integer_and_neutral_element_the_result_is_negative(self):
+        negative_int = -100
+        neutral_element = 0
+        result = sum(negative_int, neutral_element)
+
+        assert result == -100
